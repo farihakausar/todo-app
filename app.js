de69b377800b90dfdf8bc25c82747213f7ded440
farihakausar <88570666+farihakausar@users.noreply.github.com>
2022-04-24 00:48:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+var addItem = document.getElementById("addItem");
+var delItem = document.getElementById("delItem");
+var list = document.getElementById("list");
+addItem.addEventListener("click", function thisis() {
+  
+var input1 = document.getElementById("input1");
+var li = document.createElement("li");
+  var litext = document.createTextNode(input1.value);
+  li.appendChild(litext);
+  var list = document.getElementById("list");
+  list.appendChild(li);
+  input1.value = "";
+  var delbtn = document.createElement("button");
+  var deltext = document.createTextNode("Delete task");
+  delbtn.appendChild(deltext);
+  delbtn.setAttribute("class","red");
+  delbtn.setAttribute("onclick","delfunction(this);");
+  
+  li.appendChild(delbtn);
+  
+  list.appendChild(li);
+ 
+  var editbtn = document.createElement("button");
+  var edittext = document.createTextNode("edit task");
+  editbtn.appendChild(edittext);
+  
+  editbtn.setAttribute("onclick","editfunction(this);");
+  li.appendChild(editbtn);
+  
+  list.appendChild(li);
+  list.style.fontSize="50px";
+  delbtn.style.display = "inline-block";
+  
+  editbtn.style.display = "inline-block";
+  delbtn.style.margin="7px";
+});
+function deltask(){
+
+  var list = document.getElementById("list");
+  list.innerHTML="";
+}
+
+function delfunction(e){
+  var addItem = document.getElementById("addItem");
+var delItem = document.getElementById("delItem");
+var list = document.getElementById("list");
+e.parentNode.remove();
+}
+function editfunction(e){
+  var edit=e.parentNode.firstChild.nodeValue;
+  var editall=prompt("are u sure you want to replace task",edit);
+  e.parentNode.firstChild.nodeValue=editall;
+}
